14b662e9142744525655905e61496474aa42383d
Eric Irvine <ericirvine@gmail.com>
2024-11-26 21:51:48 -0500

    use relative paths

--- get-installed.sh
+++ get-installed.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 echo "Saving list of installed flatpaks..."
-flatpak list --columns=application,origin --app | grep "flathub" | awk '{print $1}' > /var/home/eric/.dotfiles/installed/flatpaks.txt
+flatpak list --columns=application,origin --app | grep "flathub" | awk '{print $1}' > flatpaks.txt
 
-cd /var/home/eric/.dotfiles/installed/
 git add flatpaks.txt
 git commit -m "Update Flatpak List $(date)"
 
 echo "Saving list of installed gnome extensions..."
-gnome-extensions list --enabled > /var/home/eric/.dotfiles/installed/extensions.txt
+gnome-extensions list --enabled > extensions.txt
 
 git add extensions.txt
 git commit -m "Update Gnome Extensions List $(date)"
 
 echo "Saving list of installed brew programs..."
-brew bundle dump --file=/var/home/eric/.dotfiles/installed/Brewfile.txt --force
+brew bundle dump --file=Brewfile.txt --force
 
 git add Brewfile.txt
 git commit -m "Update Brewfile list $(date)"
