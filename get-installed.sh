7879d0514f8bfdcc5108a8dc30516089ba5e125e
Eric Irvine <ericirvine@gmail.com>
2024-11-26 21:48:36 -0500

    new echo commands

--- get-installed.sh
+++ get-installed.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+echo "Saving list of installed flatpaks..."
 flatpak list --columns=application,origin --app | grep "flathub" | awk '{print $1}' > /var/home/eric/.dotfiles/installed/flatpaks.txt
 
 cd /var/home/eric/.dotfiles/installed/
 git add flatpaks.txt
 git commit -m "Update Flatpak List $(date)"
 
+echo "Saving list of installed gnome extensions..."
 gnome-extensions list --enabled > /var/home/eric/.dotfiles/installed/extensions.txt
 
 git add extensions.txt
 git commit -m "Update Gnome Extensions List $(date)"
 
+echo "Saving list of installed brew programs..."
 brew bundle dump --file=/var/home/eric/.dotfiles/installed/Brewfile.txt --force
 
 git add Brewfile.txt
 git commit -m "Update Brewfile list $(date)"
 
+echo "Pushing to github"
 git push -u origin main
