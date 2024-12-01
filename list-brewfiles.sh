d0781918a6dfdf365c6a1ef6e56861d9c5b73ac2
Eric Irvine <ericirvine@gmail.com>
2024-11-30 23:26:03 -0500

    Update Flatpak List Sat 30 Nov 2024 11:26:03 PM EST

--- list-brewfiles.sh
+++ list-brewfiles.sh
@@ -1,5 +1,5 @@
 cd /var/home/eric/.dotfiles/installed/
-/var/home/linuxbrew/.linuxbrew/Homebrew/bin/brew bundle dump --force 2>&1 | tee brewout.txt | logger
+/home/linuxbrew/.linuxbrew/bin/brew bundle dump --force 2>&1 | tee brewout.txt | logger
 
 git add Brewfile
 git commit -m "Update Brewfile list $(date)"
