31bb0cfd1453f019e42aa6032fa8db2de007d800
Eric Irvine <ericirvine@gmail.com>
2024-11-30 23:28:46 -0500

    Update Gnome Extensions List Sat 30 Nov 2024 11:28:46 PM EST

--- list-brewfiles.sh
+++ list-brewfiles.sh
@@ -1,5 +1,7 @@
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 cd /var/home/eric/.dotfiles/installed/
-/home/linuxbrew/.linuxbrew/bin/brew bundle dump --force 2>&1 | tee brewout.txt | logger
+brew bundle dump --force 2>&1 | tee brewout.txt | logger
 
 git add Brewfile
 git commit -m "Update Brewfile list $(date)"
